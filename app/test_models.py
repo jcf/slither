fd2250b7edef72084b946d2e06e1fb2248607a29
James Conroy-Finn <git@invetica.co.uk>
2024-05-10 10:51:25 +0100

    Parameterize model tests

--- app/test_models.py
+++ app/test_models.py
@@ -4,13 +4,13 @@ from pydantic import ValidationError
 from .models import Farm, crops
 
 
-def test_valid_crops():
-    for crop in crops:
-        farm = Farm(id=1, crop=crop)
-        assert farm.crop == crop
+@pytest.mark.parametrize("crop", crops)
+def test_valid_crops(crop):
+    farm = Farm(id=1, crop=crop)
+    assert farm.crop == crop
 
 
-def test_invalid_crop():
-    for invalid in [None, "", "chocolate"]:
-        with pytest.raises(ValidationError):
-            Farm(id=1, crop=invalid)
+@pytest.mark.parametrize("invalid", [None, "", "chocolate"])
+def test_invalid_crop(invalid):
+    with pytest.raises(ValidationError):
+        Farm(id=1, crop=invalid)
